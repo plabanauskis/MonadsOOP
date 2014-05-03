12a5f28af611810b16c3580d5d3fd93fdf25fc1b
Paulius Labanauskis <paulius.lab@gmail.com>
2014-05-03 21:39:30 +0300

    ApplyFunction for int.

---
+++ Monads/SimpleApplicationForInt32.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Monads
+{
+    public static class SimpleApplicationForInt32
+    {
+        public static Nullable<int> ApplyFunction(
+            Nullable<int> nullable,
+            Func<int, int> function)
+        {
+            if (nullable.HasValue)
+            {
+                int unwrapped = nullable.Value;
+                int result = function(unwrapped);
+                return new Nullable<int>(result);
+            }
+            else
+            {
+                return new Nullable<int>();
+            }
+        }
+    }
+}
