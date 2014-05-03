f18e530f006c3d88eaecbb268fcb13ac7fac4ce6
Paulius Labanauskis <paulius.lab@gmail.com>
2014-05-03 22:19:31 +0300

    Monadic ApplyFunction.

---
+++ Monads/MonadicApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Monads
+{
+    public static class MonadicApplication
+    {
+        static Nullable<R> ApplyFunction<A, R>(
+            Nullable<A> nullable,
+            Func<A, R> function)
+            where A : struct
+            where R : struct
+        {
+            if (nullable.HasValue)
+            {
+                A unwrapped = nullable.Value;
+                R result = function(unwrapped);
+                return new Nullable<R>(result);
+            }
+            else
+            {
+                return new Nullable<R>();
+            }
+        }
+    }
+}
